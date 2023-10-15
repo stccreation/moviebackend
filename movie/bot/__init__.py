9b727ca4f38373dc076889db571d0f0e18fa622b
Shaheen T <51311198+stccreation@users.noreply.github.com>
2023-10-15 17:48:41 +0530

    Update __init__.py

--- movie/bot/__init__.py
+++ movie/bot/__init__.py
@@ -3,7 +3,7 @@ from pyrogram import Client
 
 Bot = Client(
     name="Movie",
-    api_id=1383392,
-    api_hash='f7ec7a1bc8c41c6fe23c2706c67eda7c',
+    api_id=000000,
+    api_hash='abdbbfsbfbhdhgdf',
     sleep_threshold=60,
     workers=4)
